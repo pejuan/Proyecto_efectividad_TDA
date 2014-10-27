2ee5a6f7bdf8dde21ca883d8b57819568a4df0f5
Jose Pejuan <jose.pejuan@unitec.edu>
2014-10-26 22:00:02 -0600

    fsarraylist

--- fsarraylist.h
+++ fsarraylist.h
@@ -13,25 +13,25 @@
 
 class fsarrayList : public TDAList
 {
-public:
-fsarrayList(int);
-virtual ~fsarrayList();
-virtual bool insert(Object*,int);
-virtual Object* remove(unsigned int);
-virtual Object* first() const;
-virtual Object* last() const;
-virtual int getCapacity() const;
-virtual bool isFull() const;
-virtual void clear();
-virtual Object* get(unsigned int) const;
-virtual void print()const;
-virtual int indexOf(Object* E) const ;/////////////
-virtual int prev(int) const;
-virtual int next(int) const;
+ private:
+	unsigned int capacity;
+	Object** data;
+ public:
+	fsarrayList(int);
+	virtual ~fsarrayList();
+	virtual bool insert(Object*,int);
+	virtual Object* remove(unsigned int);
+	virtual Object* first() const;
+	virtual Object* last() const;
+	virtual int getCapacity() const;
+	virtual bool isFull() const;
+	virtual void clear();
+	virtual Object* get(unsigned int) const;
+	virtual void print()const;
+	virtual int indexOf(Object* E) const ;
+	virtual int prev(int) const;
+	virtual int next(int) const;
 
-private:
-unsigned int capacity;
-Object** data;
 };
 
 
