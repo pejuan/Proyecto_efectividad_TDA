c68390499f3c80dad7ba07a721bec54190c1b972
Jose Pejuan <jose.pejuan@unitec.edu>
2014-10-26 22:34:42 -0600

    modificado main

--- main.cpp
+++ main.cpp
@@ -25,32 +25,20 @@ int main(int argc, char** argv) {
 	TDAList* lista;
 	int pos,elemento;
 	clock_t initial,final;
-	//lista = new fsarrayList(5000);
+	lista = new fsarrayList(5000);
 	//lista = new cursor(5000);
-	lista = new SLinkedList();
+	//lista = new SLinkedList();
 	srand(time(0));
 	for (int i = 0; i < 5000; ++i){
 		pos = (rand() % (int)(lista->size()+1));
 		elemento = rand()%5000;
 		lista->insert(new Integer(elemento),pos);
 		
-		//final = clock();
-		//cout<<final<<" ";
-	}
-	/*initial = clock();
-	Object* e;
-	cout<<"antes for grande"<<endl;
-	for (int i = 0; i < 5000; ++i)
-	{
-		
-		
-		pos = (rand() % (int)(lista->size()+1));
-		e = lista->get(pos);
-		final = clock()-initial;
+		final = clock();
 		cout<<final<<" ";
-
-
-	}*/
+	}
+	initial = clock();
+	Object* e;
 
 	cout<<"SIZE:"<<lista->size()<<endl;
 	delete lista;
